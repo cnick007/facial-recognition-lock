d54b8f25f54a43fbadf1f5982f7762ba8e069365
cnick007 <cnick007@fiu.edu>
2023-03-19 22:04:40 -0400

    add gpio support to work with solenoid

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from neural_recognizer import Recognizer
 import cv2
-import os
+from time import sleep
+import RPi.GPIO as GPIO
 
 
 def main():
     recognizer = Recognizer()
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.output(18, 1)
 
     print("Starting scan...")
     while True:
@@ -12,7 +17,9 @@ def main():
 
         # check if a face match was found
         if recognizer.face_match:
+            GPIO.output(18, 0)
             print("Face Match!")
+            sleep(10)
 
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
